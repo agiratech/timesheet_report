8cb9fd89b026a976e93dfc14ce1186c92e76a8b7
Saravana <saran@agiratech.com>
2016-12-17 18:44:44 +0530

    Project specific charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -5,19 +5,16 @@ class ChartsController < ApplicationController
 
   def index
     if @reports.present?
-      if params[:chart_type] == "project"
-        project_specific_chart
-      else
-        user_specific_chart
-      end
+      project_specific_chart
+      user_specific_chart
     end
   end
 
   private
 
   def project_specific_chart
-    @chart1_title = 'Time Spent vs Users'
-    @chart2_title = 'Daily Time Spent vs Users'
+    @chart1_title = 'Time Spent vs Projects'
+    @chart2_title = 'Daily Time Spent vs Projects'
     # ************************** Time Spent vs Users **************************
     users_hash = {}
     @users.map {|user| users_hash[user.id] = user.firstname.capitalize + " " + user.lastname }
@@ -32,7 +29,7 @@ class ChartsController < ApplicationController
       end
     end
     overall_time_spent.each do |key, value|
-      @overall_time_spent_result << [users_hash[key], value.round(1)]
+      @overall_project_time_spent_result << [users_hash[key], value.round(1)]
     end
     # ************************** Daily Time Spent vs Users **************************
     daily_time_spent_vs_users(users_hash)
@@ -55,13 +52,12 @@ class ChartsController < ApplicationController
         daily_time_spent[hour["user"]][hour["spent_on"].to_s] = hour["hours"].to_f + daily_time_spent[hour["user"]][hour["spent_on"].to_s].to_f
       end
     end
-    # binding.pry
     daily_time_spent.each do |user_id, time_spent|
       daily_spent = []
       dates.each do |date|
         daily_spent << time_spent[date.to_s].to_f.round(1)
       end
-      @date_wise_spent << {
+      @date_wise_project_spent << {
         name: users_hash[user_id],
         data: daily_spent
       }
@@ -69,18 +65,18 @@ class ChartsController < ApplicationController
   end
 
   def user_specific_chart
-    @chart1_title = 'Time Spent vs Projects'
-    @chart2_title = 'Daily Time Spent vs Projects'
+    @chart3_title = 'Time Spent vs Users'
+    @chart4_title = 'Daily Time Spent vs Users'
     # ************************** Time spent vs Projects **************************
     overall_time_spent = Hash.new
     @reports.hours.each do |hour|
       overall_time_spent[hour[@custom_filter_type]] = overall_time_spent[hour[@custom_filter_type]].to_f + hour["hours"].to_f
     end
     if params[:custom_project_name].present?
-      @overall_time_spent_result << [params[:custom_project_name], overall_time_spent[params[:custom_project_name]].to_f.round(1)] if overall_time_spent[params[:custom_project_name]]
+      @overall_user_time_spent_result << [params[:custom_project_name], overall_time_spent[params[:custom_project_name]].to_f.round(1)] if overall_time_spent[params[:custom_project_name]]
     else
       overall_time_spent.each do |key, value|
-        @overall_time_spent_result << [key, value.round(1)]
+        @overall_user_time_spent_result << [key, value.round(1)]
       end
     end
     # ***** Daily Time spent vs Projects *****
@@ -97,12 +93,12 @@ class ChartsController < ApplicationController
         daily_spent << time_spent[date.to_s].to_f.round(1)
       end
       if params[:custom_project_name].present? && project_name == params[:custom_project_name]
-        @date_wise_spent << {
+        @date_wise_user_spent << {
           name: project_name,
           data: daily_spent
         }
       elsif !params[:custom_project_name].present?
-        @date_wise_spent << {
+        @date_wise_user_spent << {
           name: project_name,
           data: daily_spent
         }
@@ -122,8 +118,10 @@ class ChartsController < ApplicationController
   def initialize_defaults
     @custom_project_field = CustomField.where("type =? AND name =?", "TimeEntryCustomField", "Project Name").first
     if @custom_project_field.present?
-      @date_wise_spent = []
-      @overall_time_spent_result = Array.new
+      @date_wise_user_spent = []
+      @overall_user_time_spent_result = Array.new
+      @date_wise_project_spent = []
+      @overall_project_time_spent_result = Array.new
       @custom_filter_type = "cf_#{@custom_project_field.id}"
       @date = Date.today
       @from = params[:from_date].present? ? params[:from_date].to_date : @date.beginning_of_month
@@ -131,11 +129,9 @@ class ChartsController < ApplicationController
       @current_user = find_current_user
       user_id = @current_user.admin ? params[:user_id] :  @current_user.id
       @users = User.where("type =?", "User").order("login ASC")
-      puts user_id
       @query = TimeEntryQuery.build_from_params(params, :project => @project, :name => '_')
       scope = time_entry_scope.where(:spent_on => @from..@to)
       scope = scope.where("user_id =?", user_id) if user_id.present?
-      # @report = Redmine::Helpers::TimeReport.new(@project, @issue, [@custom_filter_type], "month", scope)
       @reports = Redmine::Helpers::TimeReport.new(@project, @issue, ["user", @custom_filter_type], "day", scope)
     end
   end
